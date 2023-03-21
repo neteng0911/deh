b541a8dbc8012e6ec7435bcd5ba16add42b997a9
Evan_pc <fashion0002@gmail.com>
2023-03-21 15:52:06 +0200

    trying columns

--- columns.py
+++ columns.py
@@ -1,3 +1,14 @@
+import os
+
+if os.path.exists("columnsout2.txt"):
+  os.remove("columnsout2.txt")
+else:
+  print("The file does not exist")
+if os.path.exists("columnsout3.txt"):
+  os.remove("columnsout3.txt")
+else:
+  print("The file does not exist")
+
 fin = open('columns.txt','rt',encoding="utf8" )
 fout = open ('columnsout.txt', 'wt')
 for line in fin:
@@ -7,9 +18,25 @@ fout.close()
 
 
 fout = open('columnsout.txt', 'r')
-fout2 = open ('columnsout2.txt', 'wt')
+fout2 = open ('columnsout2.txt', 'w+')
 for line in fout:
-    fout2.write(line.replace('\t\t', ',""'))
+    fout2.write(line.replace('\t\t', ','))
+
+
+fout2.close()
+fout2 = open ('columnsout2.txt', 'r')
+fout3 = open('columnsout3.txt', 'w+')
+colstr=fout2.read().split(',')
+print (colstr)
+print(len(colstr))
+
+
+fout3.write(str(colstr))
+
+
+
+
 fin.close()
 fout.close()
 fout2.close()
+fout3.close()
--- indices.py
+++ indices.py
@@ -1,6 +1,6 @@
 fin = open('ind.txt','rt' )
 fout = open ('indices.txt', 'wt')
 for line in fin:
-    fout.write(line.replace('\t',','))
+    fout.write(line.replace('\t',' '))
 fin.close()
 fout.close()
